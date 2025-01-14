973e00b865b9badd4d68e2a0f3a744c7cdae8ef8
nhminn <itzthatminn@gmail.com>
2025-01-14 22:11:30 +0700

    update localization

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -13,7 +13,7 @@ export default function AboutScreen() {
                 <div className={cls.Title}>
                     <h1>luckit</h1>
                     <p>
-                        unofficial Locket extension to save your friends' moments.
+                        an unofficial Locket extension to show live photos from your friends.
                     </p>
                     <p className={cls.Version}>
                         version: {VERSION}
@@ -23,11 +23,11 @@ export default function AboutScreen() {
             <div className={cls.Disclaimer}>
                 <h2>Disclaimer</h2>
                 <p>
-                    This project is not affiliated with Locket or Locket Labs, Inc in anyway. By using this extension, you acknowledge that it is an unofficial Locket client, and you accept the risk that your account may be banned.
+                    This project is not affiliated with Locket or Locket Labs, Inc in any way. By using this extension, you acknowledge that it is an unofficial Locket client, and you accept the risk that your account may be banned.
                     <br />
-                    If you are not comfortable with this or you don't know what you are doing, please do not use this extension and remove it from your browser.
+                    If you're unsure about this or you don't know what you are doing, please refrain from using this extension.
                     <br />
-                    I (creator of this extension) will not be held responsible for any consequences.
+                    I (luckit's creator) won't be held responsible for any consequences.
                 </p>
             </div>
             <div className={cls.Bruh}>
